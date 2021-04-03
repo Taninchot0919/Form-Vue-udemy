aceabbf40a7a964ae860c43201274fb42664afb8
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-04-03 12:32:41 +0700

    [UPDATE] END Basic Form Vue

--- src/components/Signup-form.vue
+++ src/components/Signup-form.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="signup-form">
-    <form>
+    <form @submit.prevent="handleSubmitEvent">
       <label>Email :</label>
       <input type="email" required v-model="email" />
 
       <label>Password :</label>
       <input type="password" required v-model="password" />
+        <div class="error" v-if="passwordError">
+            {{passwordError}}
+        </div>
 
       <label>Role :</label>
       <select v-model="role">
@@ -13,11 +16,20 @@
         <option value="designer">Web Designer</option>
       </select>
 
+      <label>Skills (if you have more than 1 skill you must alt + , to add new skill):</label>
+      <input type="text" v-model="tempSkill" @keydown="addSkill" />
+      <div v-for="skill in skills" :key="skill" class="pill" >
+        <!-- เราสามารถใช้ skill จาก v-for ที่วนลูปได้เลย เพราะมันรู้ว่าแต่ละตัวอยู่ไหน -->
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
+      </div>
       <div class="terms">
         <input type="checkbox" required v-model="terms" />
         <label>Accept terms and conditions</label>
       </div>
-    <!--  
+      <div class="submit">
+          <button>Create an Account</button>
+      </div>
+      <!--  
       /** ถ้าหากมี checkbox หลายช่องจะต้องใส่ value ลงไปด้วยเพื่อระบุว่าช่องๆนั้นคืออะไร
       <div>
         <input type="checkbox" value="Luigi" v-model="names" />
@@ -47,8 +59,45 @@ export default {
       password: "",
       role: "designer",
       terms: false,
-    //   names: []
+      skills: [],
+      tempSkill: "",
+      passwordError : '',
+      //   names: []
     };
+  },
+  methods: {
+    addSkill(e) {
+    //   console.log(e);
+      if (e.key === "," && this.tempSkill) {
+        //   Check every array in skills that have tempSkill
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        //   console.log(this.skills);
+        }
+          this.tempSkill = "";
+
+      }
+    },
+    deleteSkill(skill){
+        this.skills = this.skills.filter((item)=>{
+            // ถ้า skill ที่รับเข้ามาไม่ตรงกับ item จะ return true ทำให้ยังเก็บไว้
+            // แต่ถ้า skill ที่รับเข้ามาเท่ากับ item จะ return false (filter ทำให้สิ่งที่ return false หายไป)
+            return skill!==item
+        })
+    },
+    handleSubmitEvent(){
+        console.log('Form Submit')
+        // Validate Password
+        // เหมือนเป็นการทำ if แบบสั้นๆ         ด้านซ้ายจะเป็นTrue    ด้านขวาเป็น false
+        this.passwordError = this.password.length > 5 ? '' : 'Password must at least 6 characters long'
+        if(!this.passwordError){
+            console.log(this.email)
+            console.log(this.password)
+            console.log(this.role)
+            console.log(this.terms)
+            console.log(this.skills)
+        }
+    }
   }
 };
 </script>
@@ -87,5 +136,35 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill{
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+}
+button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+    cursor: pointer;
+}
+.submit {
+    text-align: center;
+}
+.error {
+    color: #ff0a62;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
